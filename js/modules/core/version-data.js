ab527bdb720bf2ca9fe79dd3e150edb15b5688cd
JRKy <josh.kennedy@gmail.com>
2025-04-10 19:32:03 -0600

    chore: update version to %s

--- js/modules/core/version-data.js
+++ js/modules/core/version-data.js
@@ -1,10 +1,10 @@
 // version-data.js - Version information as a JavaScript module
 export const versionData = {
-  version: "2.4.7",
+  version: "2.4.8",
   buildDate: "2025-04-11",
   versionInfo: {
     major: 2,
     minor: 4,
-    patch: 7
+    patch: 8
   }
 };
