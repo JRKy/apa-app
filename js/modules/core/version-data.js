9279a28d0f3e46b7bce99bd5a9d2d13eb4227383
JRKy <josh.kennedy@gmail.com>
2025-04-10 19:21:26 -0600

    chore: prepare for version update test

--- js/modules/core/version-data.js
+++ js/modules/core/version-data.js
@@ -1,10 +1,10 @@
 // version-data.js - Version information as a JavaScript module
 export const versionData = {
-  version: "2.4.5",
-  buildDate: "2024-04-10",
+  version: "2.4.6",
+  buildDate: "2024-04-11",
   versionInfo: {
     major: 2,
     minor: 4,
-    patch: 5
+    patch: 6
   }
 }; 
